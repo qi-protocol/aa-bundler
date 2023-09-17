124973694ee1d2683d66b5d08516c108c3088cde
da-bao-jian <gaohaoyuandd@gmail.com>
2023-09-16 20:12:19 -0400

    seriliaze and deserialize uopool mode

--- crates/primitives/src/uopool.rs
+++ crates/primitives/src/uopool.rs
@@ -3,7 +3,9 @@ use serde::{Deserialize, Serialize};
 use strum_macros::{EnumString, EnumVariantNames};
 
 /// Verification modes for user operation mempool
-#[derive(Clone, Copy, Debug, EnumString, EnumVariantNames, PartialEq, Eq)]
+#[derive(
+    Clone, Serialize, Deserialize, Copy, Debug, EnumString, EnumVariantNames, PartialEq, Eq,
+)]
 #[strum(serialize_all = "kebab_case")]
 pub enum Mode {
     Standard,
